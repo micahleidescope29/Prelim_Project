23ed963491d901855565cd0e25695c39e77100b6
Micah Novilla <kneejerk_sluggard@yahoo.com>
2017-01-07 22:08:47 +0800

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of the routes that are handled
+| by your application. Just tell Laravel the URIs it should respond
+| to using a Closure or controller method. Build something great!
+|
+*/
+Route::group(['middleware' => ['web']], function(){
+	Route::get('/', function () {
+    return view('welcome');
+	});
+
+	Route::post('/signup', [
+		'uses' => 'UserController@postSignUp', 
+		'as' => 'signup'
+	]);
+	Route::post('/signin', [
+		'uses' => 'UserController@postSignIn', 
+		'as' => 'signin'
+	]);
+
+	Route::get('/dashboard', [
+		'uses' => 'UserController@getDashboard', 
+		'as' => 'dashboard'
+	]);
+
+});
+
+
