3fe2866f7bfcfeaeb8f4e51248b2916660f977dc
Micah Novilla <kneejerk_sluggard@yahoo.com>
2017-01-13 15:58:48 +0800

    Made a profile page partially

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -12,18 +12,37 @@
 <section class="row new-post">
 	<div class="col-md-6 col-md-offset-3">
 		<header><h3><center>PROFILE</center></h3></header>
-			<form action="{{ route('account.save') }}" method="post" enctype="multipart/form-data">
 
 
-
-				@if (Storage::disk('local')->has($user->first_name . '-' . $user->id . '.jpg'))
+			@if (Storage::disk('local')->has($user->first_name . '-' . $user->id . '.jpg'))
 					<section class="row new-post">
 						<div class="col-md-6 col-md-offset-3">
 							<img src="{{ route('account.image', ['filename' => $user->first_name . '-' . $user->id . '.jpg']) }}" alt="" class="img-responsive">
 						</div>
 					</section>
-				@endif	
+			@endif	
+			<br>
+			<section>
+			<div class="col-sm-12">
+		        <div class="panel panel-info">
+		            <div class="panel-heading">
+		              <h3 class="panel-title">Profile</h3>
+		            </div>
+		            <div class="panel-body">
+		              <h4><b>First Name: </b></h4>
+		              <h4><b>Last Name: </b></h4>
+		              <h4><b>Email: </b></h4>
+		              <h4><b>Phone Number: </b></h4>
 
+		            </div>
+		        </div>
+		    </div>
+		    </section>
+
+      		<br>
+			<form action="{{ route('account.save') }}" method="post" enctype="multipart/form-data">
+
+				<center><h3>Edit Profile</h3></center>
 
 
 				<div class="form-group">
